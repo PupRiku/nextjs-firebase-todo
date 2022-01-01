1ba273791f712f2aae7c18e868375bd81d34703b
Chris Diorio <chris.diorio12@gmail.com>
2022-01-01 13:35:40 -0600

    Setup firebase and NPM packages

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPcOrmoyBKI34ZRs-nXq8fNpni0iDKdzw",
+  authDomain: "nextjs-firebase-todo-41efa.firebaseapp.com",
+  projectId: "nextjs-firebase-todo-41efa",
+  storageBucket: "nextjs-firebase-todo-41efa.appspot.com",
+  messagingSenderId: "837953772246",
+  appId: "1:837953772246:web:289ceae5f4224c34190777",
+};
+
+const app = initializeApp(firebaseConfig);
