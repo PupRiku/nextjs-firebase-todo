9d172764773819fbc97ac67808c08a58966e29b8
Chris Diorio <chris.diorio12@gmail.com>
2022-01-01 14:53:27 -0600

    Create ToDo Form

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { Container } from "@mui/material";
+import TodoForm from "../components/TodoForm";
 import TodoList from "../components/TodoList";
 
 export default function Home() {
   return (
     <Container maxWidth="sm">
+      <TodoForm />
       <TodoList />
     </Container>
   );
